cb3f62b96f91cebd9d3ce3b5769c70723140f915
Charlie <charlieedoherty@gmail.com>
2026-03-04 22:56:45 -0600

    Lesson 1

--- scripts/src/return_objects_exercise.ts
+++ scripts/src/return_objects_exercise.ts
@@ -1,6 +1,7 @@
 import { Ed25519Keypair } from "@mysten/sui/keypairs/ed25519";
 import keyPairJson from "../keypair.json" with { type: "json" };
 import { SuiGrpcClient } from "@mysten/sui/grpc";
+import { Transaction } from "@mysten/sui/transactions";
 
 /**
  *
@@ -38,6 +39,7 @@ const main = async () => {
    *
    * Create a new Transaction instance from the @mysten/sui/transactions module.
    */
+  const tx = new Transaction();
 
   /**
    * Task 2:
@@ -50,6 +52,10 @@ const main = async () => {
    * HINT: The arguments and typeArguments arguments are optional since this function does not take
    * any arguments or type arguments.
    */
+  // const nft = `${PACKAGE_ID}::sui_nft::new`;
+  const nft = await tx.moveCall({
+    target: `${PACKAGE_ID}::sui_nft::new`,
+  });
 
   /**
    * Task 3:
@@ -60,7 +66,7 @@ const main = async () => {
    *
    * HINT: Use `suiAddress`` to transfer the object to your address.
    */
-
+  tx.transferObjects([nft], suiAddress);
 
   /**
    * Task 4:
@@ -69,6 +75,8 @@ const main = async () => {
    *
    * Print the result to the console.
    */
+  const result = await suiClient.signAndExecuteTransaction({ signer: keypair, transaction: tx });
+  console.log(result);
 
 
   /**
